0b69bc465b53dca8044ecfb441b57b8996c8011e
Jean Barry <jfvbarry@gmail.com>
2020-02-28 13:16:29 -0600

    basic line drawing

--- src/javaFractalTree.java
+++ src/javaFractalTree.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Line2D;
+import java.util.StringTokenizer;
 
 public class javaFractalTree extends JFrame {
 
@@ -12,20 +14,33 @@ public class javaFractalTree extends JFrame {
     }
 
     public void setupComponents(){
-        this.setSize(800,500);
+        this.setSize(800,600);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(new BorderLayout());
-
-        JPanel backPanel = new JPanel();
-        backPanel.setBackground(Color.black);
 
         JLabel titleLabel = new JLabel("Fractal Tree");
         titleLabel.setForeground(Color.white);
-        backPanel.add(titleLabel);
+        JPanel titlePanel = new JPanel();
+        titlePanel.setBackground(Color.black);
 
-
-        this.add(backPanel);
+        titlePanel.add(titleLabel, BorderLayout.CENTER);
+        
+        this.getContentPane().setBackground(Color.black);
+        this.add(new DrawingTool(), BorderLayout.CENTER);
+        this.add(titlePanel, BorderLayout.NORTH);
         this.setVisible(true);
     }
+
+    private class DrawingTool extends JComponent{
+        public void paint(Graphics g){
+            Graphics2D graphics = (Graphics2D) g;
+            graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+
+            Shape line = new Line2D.Double(400,600,400,350);
+            graphics.setColor(Color.GREEN);
+            graphics.setStroke(new BasicStroke(7));
+            graphics.draw(line);
+        }
+    }
 }
+
